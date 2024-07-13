86e9cf88c67e5565fc505c8f6b5298999ccb5651
David R <david2005.rosental@gmail.com>
2024-07-13 19:46:13 +0100

    Changed CV,  simplified navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,33 +1,15 @@
 import { FC } from "react";
-import "./navbar.css";
+import "./Navbar.css";
 
 const Navbar: FC = () => {
   return (
     <nav>
       <ul id="navbar">
-        <div id="left-navbar">
-          <li>
-            <a id="home" href="/">
-              David R
-            </a>
-          </li>
-          <li>
-            <a id="cv" href="/David Rosental.pdf">
-              CV
-            </a>
-          </li>
-        </div>
-        <div id="right-navbar">
-          <li>
-            <a href="/projects">Projects</a>
-          </li>
-          <li>
-            <a href="/certifications">Courses</a>
-          </li>
-          {/* <li>
-            <a href="https://blog.davidrosental.co.uk">Blog-(Soon)</a>
-          </li> */}
-        </div>
+        <li>
+          <a id="home" href="/">
+            David Rosental
+          </a>
+        </li>
       </ul>
     </nav>
   );
---
+++ src/hooks.ts
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+
+/**
+ * Code and usage adapted from:
+ * - https://fireship.io/snippets/use-media-query-hook/
+ */
+export const useMediaQuery = (query: string) => {
+  const [matches, setMatches] = useState<boolean>(false);
+
+  useEffect(() => {
+    const media = window.matchMedia(query);
+    if (media.matches !== matches) {
+      setMatches(media.matches);
+    }
+    const listener = () => setMatches(media.matches);
+    window.addEventListener("resize", listener);
+    return () => window.removeEventListener("resize", listener);
+  }, [matches, query]);
+
+  return matches;
+};
